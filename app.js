bd3ff0d01a31cc353b602901a4a4ba51ca9106c3
nikov1992 <nicolas.lopez1919@gmail.com>
2024-09-20 12:38:50 -0300

    index cargado

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import messageRoutes from './src/routes/messageRoutes.js';
 import connectDB from './db.js'; // Asegúrate de que esta ruta sea correcta
-
+import path from 'path';
 
 dotenv.config();
 const app = express();
 
+// Conectar a la base de datos
+connectDB();
 
-connectDB(); // Esto ejecutará el código de conexión
-
+// Middleware para analizar el cuerpo de las solicitudes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
-app.use(express.json());
-app.use(express.text());
+
+// Middleware para servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 // Rutas
 app.use('/api/messages', messageRoutes);
--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { receiveMessage, notifyUser } from '../controllers/messageControler.js';
+
+// Obtener __dirname en contexto de módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const router = express.Router();
 
 // Ruta para recibir mensajes de Twilio
 router.post('/webhook', receiveMessage);
+
+// Ruta para servir el archivo index.html
+router.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 // Ruta para notificar al usuario cuando su pedido esté listo
 router.post('/notify', notifyUser);
