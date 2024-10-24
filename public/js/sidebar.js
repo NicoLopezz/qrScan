8f07bb08c9677e77487283efcb30b12cc03dfc1e
nikov1992 <nicolas.lopez1919@gmail.com>
2024-10-24 18:17:19 -0300

    more front fixesa

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('.menu-item').forEach(item => {
+    // Seleccionar todos los elementos del menú
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
         item.addEventListener('click', function() {
             const sectionId = this.getAttribute('data-section');
+            
+            // Remover la clase 'active' de todos los elementos del menú
+            menuItems.forEach(i => i.classList.remove('active'));
+
+            // Agregar la clase 'active' solo al ítem clicado
+            this.classList.add('active');
+
+            // Mostrar la sección correspondiente
             document.querySelectorAll('.section').forEach(section => {
                 section.classList.remove('active');
             });
