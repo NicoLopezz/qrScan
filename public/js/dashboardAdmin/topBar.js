e294c0469a6eb47e1aacf3c70e087ea307d06470
nikov1992 <nicolas.lopez1919@gmail.com>
2024-12-11 09:56:13 -0300

    modificacion de mensajes aceptado4

--- public/js/dashboardAdmin/topBar.js
+++ public/js/dashboardAdmin/topBar.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     cargarNombreLocal();
 
 
-    window.addEventListener('scroll', function() {
-        const topBar = document.querySelector('.topBar');
-        if (window.scrollY > 10) {
-            topBar.style.opacity = '0.0'; // Oculta la barra cuando scroll > 10px
-        } else {
-            topBar.style.opacity = '1'; // Muestra la barra cuando scroll <= 10px
-        }
-    });
+    // window.addEventListener('scroll', function() {
+    //     const topBar = document.querySelector('.topBar');
+    //     if (window.scrollY > 10) {
+    //         topBar.style.opacity = '0.0'; // Oculta la barra cuando scroll > 10px
+    //     } else {
+    //         topBar.style.opacity = '1'; // Muestra la barra cuando scroll <= 10px
+    //     }
+    // });
 });
 
 
